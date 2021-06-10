ec9d962f36a2235e505f2f7a5af0a5f473eb6701
Engelbert Estrada <engelbertestrada201930200@cunoc.edu.gt>
2021-06-10 02:18:26 -0600

    Ejercicio 2 parte 2

---
+++ Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Ejercicio2 {
+
+	public static void main (String [] args){
+
+		int numero;
+
+		Scanner entrada = new Scanner (System.in);
+
+		System.out.print("Ingresa un valor: ");
+		numero =  entrada.nextInt();
+
+		for ( int i = numero  ;  i > 0 ; i--){
+
+				System.out.println (i);
+
+		}
+
+
+
+
+	}
+
+
+
+
+}
